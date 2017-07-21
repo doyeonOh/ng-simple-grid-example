2e272852ac43fe62a5bc61ec5a7cda06c91364ea
doyeon oh <doyeon.oh@traport.com>
2017-07-21 17:23:11 +0900

    chore: v0.1.7 버전으로 예제 작업

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ export class AppComponent {
 
   ngSimpleGrid: any = {
     columns: [
-      { type: 'text',   key: 'id',     name:'아이디', nullValue: '1'},
-      { type: 'text',   key: 'name',   name:'이름',   nullValue: '2'},
-      { type: 'button', key: 'email',  name:'이메일', nullValue: '3', 
+      { type: 'text',   key: 'id',     name:'ID', nullValue: '1'},
+      { type: 'text',   key: 'name',   name:'NAME',   nullValue: '2'},
+      { type: 'button', key: 'email',  name:'EMAIL', nullValue: '3', 
         onClick: (e: any, value: any, data: any, index: number) => {
           alert(value);
         }},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
-import { GridModule } from 'ng-simple-grid';
+import { NgSimpleGridModule } from 'ng-simple-grid';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -15,7 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    GridModule,
+    NgSimpleGridModule,
     AngularFireModule.initializeApp(environment.firebase, 'ng-simple-grid-example')
   ],
   providers: [],
