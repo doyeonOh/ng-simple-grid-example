3c1088e37d07f069a3c8b2f2aceef49e7e79705e
doyeon oh <doyeon.oh@traport.com>
2017-07-21 19:50:47 +0900

    chore: v0.1.8 버전으로 업데이트

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ export class AppComponent {
   @ViewChild('grid')
   grid: GridComponent;
 
-  ngSimpleGrid: any = {
+  gridConfig: any = {
     columns: [
-      { type: 'text',   key: 'id',     name:'ID', nullValue: '1'},
+      { type: 'text',   key: 'id',     name:'ID',     nullValue: '1'},
       { type: 'text',   key: 'name',   name:'NAME',   nullValue: '2'},
-      { type: 'button', key: 'email',  name:'EMAIL', nullValue: '3', 
+      { type: 'button', key: 'email',  name:'EMAIL',  nullValue: '3', 
         onClick: (e: any, value: any, data: any, index: number) => {
           alert(value);
         }},
@@ -32,12 +32,11 @@ export class AppComponent {
     }
   };
 
-  dataList: any;
+  dataList: any[];
 
   ngOnInit(): void {
     this.dataList = sampleDataList;
   }
-
 
   
 }
